9caeb501331c19c77dabff7b4d99b0630230626b
Cedric Noumbo <noumbopro@gmail.com>
2024-03-21 22:23:55 -0400

    Refactor : Change WebClient to HttpClient for web request

--- GettingStarted/SimpleWebScraper/Program.cs
+++ GettingStarted/SimpleWebScraper/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using SimpleWebScraper.Builders;
 using SimpleWebScraper.Data;
 using SimpleWebScraper.Workers;
@@ -9,7 +8,7 @@ namespace SimpleWebScraper;
 public class Program
 {
     private const string Method = "search";
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         try
         {
@@ -19,37 +18,40 @@ public class Program
             Console.Write("Please enter the craigsList category that you would like to scrape: ");
             var craigsListCat = Console.ReadLine() ?? String.Empty;
 
-            using (WebClient client = new WebClient())
-            {
-                string content = client.DownloadString(
-                    $"https://{craigsListCity.Replace(" ", String.Empty)}.craigslist.org/{Method}/{craigsListCat}");
-
-                ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
-                    .WithData(content)
-                    .WithRegex(
-                        @"<li class=""(.*?)"" title=""(.*?)"">(.*?)</li>")
+            // http request uses HttpClient
+            HttpClient httpClient = new();
+            string url = $"https://{craigsListCity.Replace(" ", String.Empty)}.craigslist.org/{Method}/{craigsListCat}";
+            
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, url);
+            using HttpResponseMessage response = await httpClient.SendAsync(message);
+            
+            string content = await response.Content.ReadAsStringAsync();
+            
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                .WithData(content)
+                .WithRegex(
+                    @"<li class=""(.*?)"" title=""(.*?)"">(.*?)</li>")
+                .WithRegexOption(RegexOptions.Singleline)
+                .WithPart(new ScrapeCriteriaPartBuilder()
+                    .WithRegex(@"<div class=""title"">(.*?)</div>") // scrape description
                     .WithRegexOption(RegexOptions.Singleline)
-                    .WithPart(new ScrapeCriteriaPartBuilder()
-                        .WithRegex(@"<div class=""title"">(.*?)</div>") // scrape description
-                        .WithRegexOption(RegexOptions.Singleline)
-                        .Build())
-                    .WithPart(new ScrapeCriteriaPartBuilder()
-                        .WithRegex(@"<a href=""(.*?)"">") // scrape href value (url)
-                        .WithRegexOption(RegexOptions.Singleline)
-                        .Build())
-                    .Build();
+                    .Build())
+                .WithPart(new ScrapeCriteriaPartBuilder()
+                    .WithRegex(@"<a href=""(.*?)"">") // scrape href value (url)
+                    .WithRegexOption(RegexOptions.Singleline)
+                    .Build())
+                .Build();
 
-                Scraper scraper = new Scraper();
-                var scrapedElements = scraper.Scrape(scrapeCriteria);
+            Scraper scraper = new Scraper();
+            var scrapedElements = scraper.Scrape(scrapeCriteria);
 
-                if (scrapedElements.Any())
-                {
-                    foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
-                }
-                else
-                {
-                    Console.WriteLine("There were no matches for the specified scrape criteria.");
-                }
+            if (scrapedElements.Any())
+            {
+                foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
+            }
+            else
+            {
+                Console.WriteLine("There were no matches for the specified scrape criteria.");
             }
         }
         catch (Exception ex)
